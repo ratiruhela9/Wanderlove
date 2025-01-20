3b0e292f6696e6eb61f8a91d0400703e6531ab5a
Rati Ruhela <ratiruhela09@gmail.com>
2025-01-20 15:05:02 +0530

    Initial commit

--- app.js
+++ app.js
@@ -108,6 +108,6 @@ app.use((err, req, res, next) => {
     // res.status(statusCode).send(message);
 });
 
-app.listen(8080, () => {
-    console.log("server is listening to port 8080");
+app.listen(3000, () => {
+    console.log("server is listening to port 3000");
 });
